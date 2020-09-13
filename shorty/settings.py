61235064a7d956990f62c5ee730fda9857b5f93c
Ramyak Mehra <rmehra_be19@thapar.edu>
2020-09-14 04:41:07 +0530

    settings.py fixed for postgres

--- shorty/settings.py
+++ shorty/settings.py
@@ -61,16 +61,29 @@ WSGI_APPLICATION = 'shorty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urlshortener',
-        'USER': 'urlshortener',
-        'PASSWORD': 'password',
-        'HOST': "localhost",
-        'PORT': 5432,
+    "default": {
+        "ENGINE": config("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": config("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": config("POSTGRES_USER", "user"),
+        "PASSWORD": config("POSTGRES_PASSWORD", ""),
+        "HOST": config("SQL_HOST", "localhost"),
+        "PORT": config("SQL_PORT", "5432"),
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
