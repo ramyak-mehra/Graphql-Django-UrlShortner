3ec015a8c50a35151d6467f53eb37d1f07ecb3c3
Ramyak Mehra <rmehra_be19@thapar.edu>
2020-09-14 02:46:31 +0530

    environment varibales fix

--- shorty/settings.py
+++ shorty/settings.py
@@ -4,12 +4,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY', cast=str)
+SECRET_KEY = config('SECRET_KEY', "default_secret_key" ,cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
