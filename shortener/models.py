af2ca8a814a14f9165452741eaa0e58f3bb6bc58
Shreshth Arora <sarora_be18@thapar.edu>
2020-09-22 21:47:42 +0530

    all url fetch control , new migrations

--- shortener/models.py
+++ shortener/models.py
@@ -5,14 +5,21 @@ from django.contrib.sites.models import Site
 from graphql import GraphQLError
 from django.db import models
 from applications.models import Application
+import uuid
+
 
 class URL(models.Model):
-    application = models.ForeignKey(Application , on_delete = models.CASCADE , null = True )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    application = models.ForeignKey(
+        Application, on_delete=models.CASCADE, null=True)
     full_url = models.URLField(unique=True)
     url_hash = models.URLField(unique=True)
     clicks = models.IntegerField(default=0)
     shortened_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{ self.application } : { self.full_url }'
 
     def clicked(self):
         self.clicks += 1
@@ -24,7 +31,7 @@ class URL(models.Model):
             current_url = Site.objects.get_current().domain
             self.shortened_url = f'{current_url}/{self.url_hash}'
         validate = URLValidator()
-        
+
         try:
             validate(self.full_url)
         except ValidationError as e:
